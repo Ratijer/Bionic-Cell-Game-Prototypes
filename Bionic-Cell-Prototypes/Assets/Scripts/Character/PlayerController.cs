68517d8b29a6be072b05f1b22b91ee3aad342ce9
Rachel A <rachelemaile@gmail.com>
2020-01-09 14:04:32 -0800

    Prototype1
    Creating the player and movement

---
+++ Bionic-Cell-Prototypes/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public bool rotate;   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (rotate)
+        //{
+        //    PlayerRotation();
+        //}
+        //else
+        //{
+            PlayerMovement();
+        //}
+    }
+
+    private void PlayerMovement()
+    {
+        // Vertical: Up/Down-Arrow, or WS-keys
+        transform.position += Input.GetAxis("Vertical") * transform.up *
+                                    (movementSpeed * Time.smoothDeltaTime);
+        // Horizontal: Left/Right-Arrow, or AD-Keys
+        transform.position += Input.GetAxis("Horizontal") * transform.right *
+                                    (movementSpeed * Time.smoothDeltaTime);
+    }
+
+    private void PlayerRotation()
+    {
+        //Rotation
+        if (Input.GetMouseButton(0)) //Left
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotationSpeed) * Time.deltaTime);
+        }
+        if (Input.GetMouseButton(1)) //Right
+        {
+            transform.Rotate(new Vector3(0f, 0f, -rotationSpeed) * Time.deltaTime);
+        }
+    }
+}

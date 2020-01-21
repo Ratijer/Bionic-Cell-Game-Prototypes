5eedec45ebee293ba9d77ecab430f54693bd9f91
Rachel A <rachelemaile@gmail.com>
2020-01-20 17:06:51 -0800

    Prototype 8

--- Bionic-Cell-Prototypes/Assets/Scripts/Character/WeaponSelection.cs
+++ Bionic-Cell-Prototypes/Assets/Scripts/Character/WeaponSelection.cs
@@ -15,7 +15,51 @@ public class WeaponSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int previousSelectedWeapon = selectedWeapon;
+
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if(selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0;
+            }
+            else
+            {
+                selectedWeapon++;
+            }
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if(selectedWeapon <= 0)
+            {
+                selectedWeapon = transform.childCount - 1;
+            }
+            else
+            {
+                selectedWeapon--;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
     }
 
     private void SelectWeapon()
@@ -27,7 +71,11 @@ public class WeaponSelection : MonoBehaviour
             {
                 weapon.gameObject.SetActive(true);
             }
-
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
         }
     }
 }
